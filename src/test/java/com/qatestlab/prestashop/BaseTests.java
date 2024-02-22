0816396fb9252c2c254ddda31b59600b58fcb0a3
sergdio <159489261+sergdio@users.noreply.github.com>
2024-02-22 14:16:53 +0200

    Update BaseTests.java
    Зміни для HW_10

--- src/test/java/com/qatestlab/prestashop/BaseTests.java
+++ src/test/java/com/qatestlab/prestashop/BaseTests.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeMethod;
 
 public class BaseTests {
     WebDriver driver;
-
+// Зміни.
     @BeforeMethod
     public void beforeMethod() {
         driver = new ChromeDriver();
